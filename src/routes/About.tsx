97bca45db136f40e0082d04de9c7727432cf6502
nylespham <nylespham@gmail.com>
2024-04-22 23:53:09 +0700

    update

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -5,7 +5,8 @@ export default function About(){
             <div className="py-5">
             <h1 className="display-5 fw-bold text-white">About me</h1>
             <div className="col-lg-6 mx-auto">
-                <p className="fs-5 mb-4">SWE @ FAAANG.</p>
+                <p className="fs-5 mb-4">I'm been working as DevOps Engineer for 3 years. My passion for building and shipping software is undefeatable. 
+                Besides work, I also help my personal clients meet their business expectations on achieving operation, management, productivit. My longterm goal is to become Platform Engineer  </p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                 <a type="button" target="_blank" href="https://www.linkedin.com/in/nyles-pham-3a06a2188/" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">My LinkedIn</a>
                 <a type="button" target="_blank" href="https://www.notion.so/nylespham/Nyles-Pham-2c39011111ce48f5b5b1db56ca819197?pvs=4" className="btn btn-outline-light btn-lg px-4">My Resume</a>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,15 @@
 import ProjectDisplay from "../components/ProjectDisplay";
 import About from "./About";
+import message from "../assets/images/code.png";
 
 export default function Home(){
     return (
         <>
         <div className="px-4 py-5 my-5 text-center bg-light">
-            <img className="d-block mx-auto mb-4" src="../assets/images/nyles.jpeg" alt="" width="72" height="57"/>
+            <img style={{"height": "36px","width": "36px"}} src={message} alt="" width="72" height="57"/>
             <h1 className="display-5 fw-bold text-body-emphasis">I'm Nyles Pham</h1>
             <div className="col-lg-6 mx-auto">
-                <p className="lead mb-4">A Software Developer that's currently performing both DevOps and Development workloads</p>
+                <p className="lead mb-4">A Cloud DevOps Engineer that's currently working on both DevOps and Development roles</p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                 <button type="button" className="btn btn-primary btn-lg px-4 gap-3">My Projects</button>
                 <button type="button" className="btn btn-outline-secondary btn-lg px-4">Go to Profile</button>
